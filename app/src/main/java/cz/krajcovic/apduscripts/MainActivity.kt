13eb9784e98971c3d6e048edc2f4eece6fddd807
Dusan Krajcovic <dkrajcovic@monetplus.cz>
2022-09-19 15:40:22 +0200

    Update project

--- app/src/main/java/cz/krajcovic/apduscripts/MainActivity.kt
+++ app/src/main/java/cz/krajcovic/apduscripts/MainActivity.kt
@@ -3,12 +3,18 @@ package cz.krajcovic.apduscripts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import cz.krajcovic.apduscripts.ui.theme.ApduScriptsTheme
 
@@ -16,9 +22,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           DefaultPreview(2)
+            SwitchPreview(2)
 
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SwitchPreview(previewVersion: Int = 3) {
+    when (previewVersion) {
+        1 -> TestTextContent()
+        2 -> MessageCard(name = "Dusan")
+        3 -> JetpackCompose()
+        else -> CreateDefaultContent()
     }
 }
 
@@ -26,6 +43,22 @@ class MainActivity : ComponentActivity() {
 fun TestTextContent() {
     ApduScriptsTheme {
         Text("Hello world")
+    }
+}
+
+@Composable
+fun JetpackCompose() {
+    ApduScriptsTheme {
+        Card {
+            val expander by remember { mutableStateOf(false) }
+            Column(Modifier.clickable { expander != expander }) {
+            Image(painterResource(R.drawable.jetpack_compose_icon))
+                AnimatedVisibility(visible = expander) {
+                    Text(text = "Jetpack compose", style = MaterialTheme.typography.h2)
+                }
+
+            }
+        }
     }
 }
 
@@ -45,14 +78,4 @@ fun CreateDefaultContent() {
 @Composable
 fun MessageCard(name: String) {
     Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview(previewVersion: Int = 2) {
-    when(previewVersion) {
-        1 -> TestTextContent()
-        2 -> MessageCard(name = "Dusan")
-        else -> CreateDefaultContent()
-    }
 }
